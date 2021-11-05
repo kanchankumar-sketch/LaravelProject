32a8e9b6d73e2b209e3bf28bf21f1740f26de896
Kanchan kumar Sharma <70506833+kanchankumar-sketch@users.noreply.github.com>
2021-11-05 19:07:13 +0530

    Initial commit

---
+++ resources/views/HomePage.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('Content')
+<h1>Content part of the application</h1>
+
+
+
+@endsection
---
+++ resources/views/userlist.blade.php
@@ -0,0 +1,3 @@
+<h1>hello user list</h1>
+<h2>i am Slahs</h2>
+<h1>teen chaar</h1>
